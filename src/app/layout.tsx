a4110339c7c63af38d81feb64cde13a4e593a0ba
Manuj Sankrit <msankrit8055@gmail.com>
2025-10-19 14:20:24 +0530

    feat: 📈 add user analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
