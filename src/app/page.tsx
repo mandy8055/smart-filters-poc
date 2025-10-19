b9c474735da48e878f7b5ca96ebed9b8b4626ba7
Manuj Sankrit <msankrit8055@gmail.com>
2025-10-19 22:53:26 +0530

    fix: 💄 add independent scroll to filters section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -110,8 +110,8 @@ export default function Home() {
       <div className='max-w-[1600px] mx-auto px-4 sm:px-6 lg:px-8 py-6'>
         <div className='grid grid-cols-1 lg:grid-cols-[280px_1fr] gap-6'>
           {/* Filter Sidebar - Desktop */}
-          <aside className='hidden lg:block'>
-            <div className='sticky top-24'>
+          <aside className='hidden lg:block h-screen sticky top-0'>
+            <div className='h-full overflow-y-auto pt-6 pb-6'>
               <FilterPanel
                 appliedFilters={appliedFilters}
                 onFilterChange={setAppliedFilters}
